b5c39397fa853ee8db547820c400aabc614bb4b9
mwongeraE <evansmwongera@gmail.com>
2016-04-27 01:06:42 +0300

    navigation activity fragment

--- app/src/main/java/com/example/mwongera/papyread/MainActivity.java
+++ app/src/main/java/com/example/mwongera/papyread/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mwongera.papyread;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,6 +11,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import papybooks.interests;
+import papybooks.main_home;
+import papybooks.profile;
+import papybooks.share;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -27,6 +34,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        FragmentManager fn = getFragmentManager();
+        fn.beginTransaction().replace( R.id.frame, new main_home()).commit();
     }
 
     @Override
@@ -42,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main2, menu);
         return true;
     }
 
@@ -64,20 +76,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+
+        FragmentManager fn = getFragmentManager();
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            fn.beginTransaction().replace(R.id.frame, new main_home()).commit();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
+            fn.beginTransaction().replace(R.id.frame, new interests()).commit();
 
         } else if (id == R.id.nav_manage) {
+            fn.beginTransaction().replace(R.id.frame, new profile()).commit();
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+            fn.beginTransaction().replace(R.id.frame, new share()).commit();
 
         }
 
