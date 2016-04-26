c6630fe028f29ec7f87f6e54680906f53c71fcbc
mwongeraE <evansmwongera@gmail.com>
2016-04-27 00:15:56 +0300

    card list

--- app/src/main/java/com/example/mwongera/papyread/Cardlistactivity.java
+++ app/src/main/java/com/example/mwongera/papyread/Cardlistactivity.java
@@ -1,7 +1,142 @@
 package com.example.mwongera.papyread;
 
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.CardThumbnail;
+import it.gmariotti.cardslib.library.view.CardListView;
+
 /**
  * Created by mwongera on 4/27/16.
  */
-public class Cardlistactivity {
+public class Cardlistactivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        int listImages[] = new int[]{R.drawable.action, R.drawable.adventure,
+                R.drawable.historical, R.drawable.romance, R.drawable.funny};
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        for (int i = 0; i<5; i++) {
+            // Create a Card
+            Card card = new Card(this);
+            // Create a CardHeader
+            CardHeader header = new CardHeader(this);
+            // Add Header to card
+            header.setTitle("Novel: " + i);
+            card.addCardHeader(header);
+
+            CardThumbnail thumb = new CardThumbnail(this);
+            thumb.setDrawableResource(listImages[i]);
+            card.addCardThumbnail(thumb);
+
+            cards.add(card);
+        }
+
+        CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(this, cards);
+
+        CardListView listView = (CardListView) this.findViewById(R.id.myList);
+        if (listView != null) {
+            listView.setAdapter(mCardArrayAdapter);
+        }
+    }
+
+    public void setSupportActionBar(Toolbar supportActionBar) {
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentManager fn = getFragmentManager();
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+            fn.beginTransaction().replace(R.id.frame, new main_home()).commit();
+
+        } else if (id == R.id.nav_gallery) {
+
+            fn.beginTransaction().replace(R.id.frame, new mybooks()).commit();
+
+        } else if (id == R.id.nav_slideshow) {
+            fn.beginTransaction().replace(R.id.frame, new interests()).commit();
+
+
+        } else if (id == R.id.nav_manage) {
+            fn.beginTransaction().replace(R.id.frame, new profile()).commit();
+
+        } else if (id == R.id.nav_share) {
+            fn.beginTransaction().replace(R.id.frame, new share()).commit();
+
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/mwongera/papyread/Main2Activity.java
+++ app/src/main/java/com/example/mwongera/papyread/Main2Activity.java
@@ -1,7 +1,54 @@
 package com.example.mwongera.papyread;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.CardThumbnail;
+import it.gmariotti.cardslib.library.view.CardView;
+
 /**
  * Created by mwongera on 4/27/16.
  */
-public class Main2Activity {
+public class Main2Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create a Card
+        Card card = new Card(this, R.layout.row_card);
+
+        // Create a CardHeader
+        CardHeader header = new CardHeader(this);
+        header.setTitle("Hello world");
+
+        card.setTitle("Simple card demo");
+        CardThumbnail thumb = new CardThumbnail(this);
+        thumb.setDrawableResource(R.drawable.ic_launcher);
+
+        card.addCardThumbnail(thumb);
+
+        // Add Header to card
+        card.addCardHeader(header);
+
+        // Set card in the cardView
+        CardView cardView = (CardView) findViewById(R.id.myList);
+        cardView.setCard(card);
+
+    }
+
+    /**
+     * Card list button clicker
+     * @param v
+     */
+    public void btnCardListDemoClick(View v){
+        Intent intent = new Intent(this, CardListActivity.class);
+        startActivity(intent);
+    }
+
+
 }
