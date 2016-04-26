f4dacc00fd49e8de14c2da5a8c3f753ccb5549a2
mwongeraE <evansmwongera@gmail.com>
2016-04-27 00:37:46 +0300

    fragment

---
+++ app/src/main/java/papybooks/profile.java
@@ -0,0 +1,28 @@
+package papybooks;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mwongera.newcard.R;
+
+/**
+ * Created by root on 3/11/16.
+ */
+public class profile extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        View rootView = inflater.inflate(R.layout.tuesday, container, false);
+
+        return rootView;
+
+    }
+
+}
